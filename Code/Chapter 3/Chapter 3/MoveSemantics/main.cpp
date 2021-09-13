02245d9424695eb4ddf216085bf3b02be32244c0
Madara <willy.mendat@googlemail.com>
2021-09-13 02:14:35 +0200

    Inhalt fertig gemacht

--- Code/Chapter 3/Chapter 3/MoveSemantics/main.cpp
+++ Code/Chapter 3/Chapter 3/MoveSemantics/main.cpp
@@ -26,9 +26,28 @@ void PrintStringView(std::string_view string) {
 	std::cout << test << std::endl;
 }
 
+template <uint32_t Base>
+struct Faculty
+{
+	static const unsigned long long result = Base * Faculty<Base - 1>::result;
+};
+
+// base case
+template<>
+struct Faculty<1>
+{
+	static const unsigned long long result = 1;
+};
+
+constexpr uint32_t Sum(uint16_t value) {
+	return value <= 1 ? 1 : (value * Sum(value - 1));
+}
+
 int main(int argc, char** argv)
 {
-	doStingReserverBenchmark(Allocations);
+	//doStingReserverBenchmark(Allocations);
+
+	std::cout << Sum(5) << std::endl;
 
 	std::cin.get();
 }
